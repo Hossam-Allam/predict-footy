0d54eb63787f04ff5edaf84f8dc4798449a632e2
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-14 22:12:31 +0300

    callbacks controller

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def github
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: "GitHub")
+      sign_in_and_redirect @user, event: :authentication
+    else
+      # If user record wasn’t saved for some reason, redirect to sign-up.
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
