7904ba86d5fb6fcefe026d3767b77f132a0b7eac
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-14 21:39:30 +0300

    getting user email

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
+  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "user:email"
   OmniAuth.config.logger = Rails.logger
 end
