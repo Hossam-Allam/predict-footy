dc6c5614b6a551a1ad113bb74bcc1b3133b67e92
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-17 23:33:20 +0300

    bootstrap dependancies

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   # Root path (handled by WelcomeController)
-  root "welcome#index"
+  root "matches#index"
 end
