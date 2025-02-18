364d53b2ad5d5e4da6e7e30e9afd16e808a42ff3
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-19 02:11:28 +0300

    league resources under my namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  # Root path (handled by WelcomeController)
+  namespace :my do
+    resources :leagues
+  end
+
   root "matches#index"
 end
