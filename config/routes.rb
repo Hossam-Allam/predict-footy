f3ff42693b99cc2e55b947990f4ae63b11b97b00
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-19 02:19:43 +0300

    defining the only routes needed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   namespace :my do
-    resources :leagues
+    resources :leagues, only: [ :index, :show, :new, :create ]
   end
 
   root "matches#index"
