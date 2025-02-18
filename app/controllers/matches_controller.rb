62567013f87a0e8ddbb5e5071d9a2814fa5ae846
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-18 19:43:08 +0300

    interating kaminari

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,5 @@
 class MatchesController < ApplicationController
   def index
-    @matches = Match.order(scheduled_at: :asc).limit(10)
+    @matches = Match.order(scheduled_at: :asc).page(params[:page]).per(10)
   end
 end
