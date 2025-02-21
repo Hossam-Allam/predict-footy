e6186598b0ccc9384c176aefdd9701da017febf8
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-21 23:05:50 +0300

    prediction index route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :matches, only: [ :index, :show ] do
-    resources :predictions, only: [ :new, :create, :update, :show ]
+    resources :predictions, only: [ :new, :create, :update, :index ]
   end
 
   root "matches#index"
