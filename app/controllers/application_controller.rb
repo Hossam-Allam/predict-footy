13619fc9d4c6b603ce5f0fc692ee43c83f829846
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-24 19:50:03 +0300

    job doesnt run on every action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  after_action :run_fetch_and_evaluate, if: :user_signed_in?
+  after_action :run_fetch_and_evaluate, if: :should_run_fetch_and_evaluate?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
+    session[:ran_fetch_and_evaluate] = false
     root_path
   end
 
@@ -14,6 +15,7 @@ class ApplicationController < ActionController::Base
 
   def run_fetch_and_evaluate
     FetchAndEvaluateJob.perform_later
+    session[:ran_fetch_and_evaluate] = true
   end
 
   protected
@@ -21,5 +23,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
+
+  private
+
+  def should_run_fetch_and_evaluate?
+    user_signed_in? && session[:ran_fetch_and_evaluate] == false
   end
 end
