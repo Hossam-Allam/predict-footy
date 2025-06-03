b062ecb0502583ea5a21ae17f2f192504e7943dd
hossam-allam <h.refaie@stu.khas.edu.tr>
2025-06-03 06:11:58 +0300

    corrected implementation of remember me

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
+
   skip_before_action :verify_authenticity_token, only: :github
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -8,8 +10,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: "GitHub")
 
-      # Pass remember_me to sign_in_and_redirect
-      sign_in_and_redirect @user, event: :authentication, remember_me: remember_me
+      remember_me(@user) if remember_me
+
+      sign_in_and_redirect @user, event: :authentication
     else
       # If user record wasn’t saved for some reason, redirect to sign-up.
       session["devise.github_data"] = request.env["omniauth.auth"]
