17b1550f35f41e3537b3361e7eb0534453e26823
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2026-08-30 17:48:32 +0300

    ordering switch

--- app/controllers/my/predictions_controller.rb
+++ app/controllers/my/predictions_controller.rb
@@ -16,7 +16,7 @@ class My::PredictionsController < ApplicationController
                                         .unscored
                                         .includes(:match)
                                         .references(:match)
-                                        .order(match: { scheduled_at: :desc })
+                                        .order(match: { scheduled_at: :asc })
                                         .page(params[:page])
                                         .per(10)
 
