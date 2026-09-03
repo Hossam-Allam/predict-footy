4e26f5127d6cefe0c69cfbf8385db7fcf907e881
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2026-09-03 16:37:05 +0300

    logical fix for validation

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -55,7 +55,7 @@ class Prediction < ApplicationRecord
   def match_must_be_upcoming
     return if match.blank?
     # require the match to be "TIMED" (hasn't started) & we also check date and time jic now
-    unless match.status == "TIMED" || match.scheduled_at > Time.current - 5.minutes
+    unless match.status == "TIMED" && match.scheduled_at > Time.current - 5.minutes
       errors.add(:base, "Predictions are closed for this match")
     end
   end
